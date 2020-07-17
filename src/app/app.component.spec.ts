568ca5024123734a7f7b2699c8c0d5ab3d49074c
sowmyasri9 <sowmyasri23164@gmail.com>
2020-07-17 16:16:08 +0530

    Login and Registration component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'meanlogin'`, () => {
+  it(`should have as title 'angularjwtauth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('meanlogin');
+    expect(app.title).toEqual('angularjwtauth');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('meanlogin app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularjwtauth app is running!');
   });
 });
